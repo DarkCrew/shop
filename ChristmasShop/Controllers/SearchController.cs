839cce99da03bdfe982ce93301b14d50d226a601
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-05-04 15:12:54 +0300

    feat: add search and styles

---
+++ ChristmasShop/Controllers/SearchController.cs
@@ -0,0 +1,47 @@
+﻿using ChristmasShop.Data;
+using ChristmasShop.Data.Models;
+using Korzh.EasyQuery.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChristmasShop.ViewModels;
+
+namespace ChristmasShop.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        private readonly AppDBContent appDBContent;
+
+        public SearchController(AppDBContent appDBContent)
+        {
+            this.appDBContent = appDBContent;
+        }
+
+        public IActionResult Search()
+        {
+            SearchViewModel model = new SearchViewModel();
+            model.trees = appDBContent.Tree.ToList();
+            return View(model);
+        }
+
+        private List<Tree> SearchEverywhere(string text) => appDBContent.Tree.FullTextSearchQuery(text).ToList();
+
+
+        [HttpPost]
+        public IActionResult Search(SearchViewModel model)
+        {
+
+            if (!string.IsNullOrEmpty(model.Text))
+            {
+                model.trees = SearchEverywhere(model.Text).ToList();
+            }
+            else model.trees = appDBContent.Tree.ToList();
+            model.trees = model.trees.Distinct().ToList();
+            return View(model);
+        }
+    }
+}
---
+++ ChristmasShop/ViewModels/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ChristmasShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChristmasShop.ViewModels
+{
+    public class SearchViewModel
+    {
+        public List<Tree> trees { get; set; }
+
+        public string Text { get; set; }
+    }
+}
